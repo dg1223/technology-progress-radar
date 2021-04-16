547cb6e39db03e9b17f6862f61202a23a3b0915d
Shamir Alavi <dg1223@users.noreply.github.com>
2021-04-16 03:07:57 -0400

    latest registry, changes to data cleanup process

--- radar.py
+++ radar.py
@@ -60,11 +60,22 @@ class preprocess:
             if pd.isna(etr.iloc[x, ind]):
                 etr.iloc[x, ind] = 'Engage'
 
+        # Remove duplicate names
+        etr.drop_duplicates(subset=['Emerging Technology'], inplace=True)
+
         # Use short form 'AI'
         etr.replace("Artificial\ Intelligence\ \(AI\)", "AI", regex=True, inplace=True)
-        etr.replace("Real\-Time\ Analytics\/Continuous\ Intelligence", "RTA / CI", regex=True, inplace=True)
+        # etr.replace("Real\-Time\ Analytics\/Continuous\ Intelligence", "RTA/CI", regex=True, inplace=True)
         etr.replace("Pan\ Canadian\ Trust\ Framework", "PCTF", regex=True, inplace=True)
         etr.replace("Enterprise\ taxonomy\ and\ ontology\ management", "Enterprise Taxonomy & Ontology", regex=True, inplace=True)
+        etr.replace("Infrastructure\ as\ a\ code\ using\ Terraform", "Infrastructure as code", regex=True, inplace=True)
+        etr.replace("Angular\ framework", "Angular", regex=True, inplace=True)
+        etr.replace("REST\ webservices", "REST", regex=True, inplace=True)
+        etr.replace("Pan\ Canadian\ Trust\ Framework", "Pan-Canadian Trust Framework", regex=True, inplace=True)
+        etr.replace("R\ &\ Python", "R \/ Python", regex=True, inplace=True)
+        etr.replace("ICAM", "Identity, Credential & Access Management", regex=True, inplace=True)
+        etr.replace("DLT Offensive", "Distributed Ledger Technology - offensive", regex=True, inplace=True)
+        etr.replace("DLT Defensive", "Distributed Ledger Technology - defensite (cryptocurrency)", regex=True, inplace=True)
         print(etr.head(10))
 
         # Reset index after dropping NaN
